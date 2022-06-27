c6065c15e5561e37e74890baa3b33fd98d519966
furkan <furkanesen35@gmail.com>
2022-06-27 04:12:02 +0200

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss"
 import Navbar from "./components/navbar/navbar";
+import Header from "./components/header/header";
 const App = () => {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
     </div>
   )
 }
